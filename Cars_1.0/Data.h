807f935bc43a8b763001b6c009ae8e213c494e93
PareNN <95346319+PareNN@users.noreply.github.com>
2024-07-12 12:28:30 +0300

    Update Data.h

--- Cars_1.0/Data.h
+++ Cars_1.0/Data.h
@@ -11,19 +11,13 @@
 struct Config {
 
 #ifdef Start_Mode
-
   int Speed = 100;  // Скорость, значение 0-255
   int Wheel = 100;
-  // Нсатройки сети
-  bool WiFimode = 0;  // 0 AP, 1 local
-
-  //char ssid[32] = "Parepko_NN";  // логин wifi
-  char ssid[32] = "";  // логин wifi
-  //char pass[32] = "28101979n";   // пароль wifi
-  char pass[32] = "";  // пароль wifi
-
+  // Настройки сети
+  bool WiFimode = 0;   // 0 AP, 1 local
+  char ssid[32] = "test";  // логин wifi
+  char pass[32] = "test";  // пароль wifi
 #else
-
   int Speed;
   int Wheel;
   bool WiFimode;  // 0 AP, 1 local
